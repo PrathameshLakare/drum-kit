ec0018e5442845c0d99958c086e2d3137a97b531
PrathameshLakare <prathameshlakare001@gmail.com>
2023-02-13 22:44:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ var numberOfButtons = document.querySelectorAll(".drum").length;
 for(var i= 0; i<numberOfButtons;i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click",function handlesClick (){
-        
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
-
-
+    this.style.color = "white";
     });   
 
 }
-
+// var audio = new Audio("sounds/tom-1.mp3");
+//         audio.play();

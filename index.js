727ce6c8c6a8cadeeec7405a7f40d3bc64830dd0
PrathameshLakare <prathameshlakare001@gmail.com>
2023-02-13 22:16:26 +0530

    style - add background images to button

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ var numberOfButtons = document.querySelectorAll(".drum").length;
 for(var i= 0; i<numberOfButtons;i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click",function handlesClick (){
-        alert("click");
+        
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+
+
     });   
 
 }
